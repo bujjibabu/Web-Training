8dc935cf058663fc5cbcf49dffbea5897ce62702
bujjibabu <bujji404@gmail.com>
2015-03-16 20:48:44 +0530

    javascript first class

---
+++ bujji/javascript/js1.js
@@ -0,0 +1,15 @@
+	/* var x = 'This bujji babu';
+		var X ='This script'; 
+		var x = 'This bujji babu';
+		var X ='This script';
+		var x = 'This bujji babu';
+		var X ='This script'; */
+		//document.write(X);
+
+		//var x = 'This bujji babu';
+		//var X ='This script'; 
+		//var x = 'This bujji babu';
+		//var X ='This script';
+		//var x = 'This bujji babu';
+		/*var X ='This is script';
+		document.write(X);*/
